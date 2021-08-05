a84e567ca2bfdcfd557cc8d4e824122971462718
seongmook lee <43744260+2012monk@users.noreply.github.com>
2021-08-05 12:57:44 +0900

    Time: 156 ms (36.88%), Space: 15.5 MB (39.52%) - LeetHub

--- number-of-islands/number-of-islands.py
+++ number-of-islands/number-of-islands.py
@@ -1,20 +1,22 @@
 class Solution:
+    grid: list[list[str]]
+        
     def numIslands(self, grid: List[List[str]]) -> int:
         m, n = len(grid), len(grid[0])
-        v = [[0 for _ in range(len(grid[0]))] for _ in range(len(grid))]
+        mr, nr = range(m), range(n)
+
         land = 0
         def dfs(i, j):
-            dx, dy = [0, 0, 1, -1], [1, -1, 0, 0]
-            for nx, ny in zip(dx, dy):
+            for nx, ny in zip([0, 0, 1, -1], [1, -1, 0, 0]):
                 x, y = i + nx, j + ny
-                if x in range(m) and y in range(n) and not v[x][y] and grid[x][y] != '0':
-                    v[x][y] = ~v[x][y]
+                if x in mr and y in nr and grid[x][y] != '0':
+                    grid[x][y] = '0'
                     dfs(x, y)
             return 1
         
         for i in range(m):
             for j in range(n):
-                if not v[i][j] and grid[i][j] == '1':
+                if grid[i][j] == '1':
                     land += dfs(i, j)
         return land
                     
