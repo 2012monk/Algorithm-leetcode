fb8b8d4ae36054725e77fbe620ebbe5a2821a2c1
seongmook lee <43744260+2012monk@users.noreply.github.com>
2021-08-09 00:00:15 +0900

    Time: 452 ms (38.96%), Space: 16.2 MB (74.92%) - LeetHub

--- trapping-rain-water-ii/trapping-rain-water-ii.py
+++ trapping-rain-water-ii/trapping-rain-water-ii.py
@@ -6,26 +6,22 @@ class Solution:
         for i in range(m):
             for j in range(n):
                 if i == 0 or j == 0 or j == n -1 or i == m -1:
-                    # v[i][j] = 1
                     heapq.heappush(q, (h[i][j], i, j))
+                    v[i][j] = 1
 
-        # mx = q[0][0]
         mx = 0
         res = 0
         while q:
             w, i, j = heapq.heappop(q)
-            if v[i][j]:
-                continue
-            v[i][j] = 1
+
             mx = max(mx, w)
             if 0<i<m-1 and 0<j<n-1 and w <= mx:
                 res += mx - w
             
-            print(f'current {w} max {mx} {i,j} res {res}')
-            
             for dx, dy in zip([0,0,1,-1], [1,-1,0,0]):
                 x, y = i + dx, j + dy
                 if 0<x<m and 0<y<n and not v[x][y]:
+                    v[x][y] = 1
                     heapq.heappush(q, (h[x][y], x, y))
             
                 
