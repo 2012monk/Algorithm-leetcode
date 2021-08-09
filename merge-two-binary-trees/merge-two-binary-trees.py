45ee4237d5e87b7a62f0e389ec717239b547cda7
seongmook lee <43744260+2012monk@users.noreply.github.com>
2021-08-09 16:32:09 +0900

    Time: 84 ms (76.89%), Space: 15.5 MB (24.21%) - LeetHub

--- merge-two-binary-trees/merge-two-binary-trees.py
+++ merge-two-binary-trees/merge-two-binary-trees.py
@@ -6,12 +6,6 @@
 #         self.right = right
 class Solution:
     def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
-        if not root1:
-            return root2
-        if not root2:
-            return root1
-        
-        dummy = tmp = TreeNode();
         def merge(node1, node2):
             if node1 and node2:
                 node1.val += node2.val
@@ -23,5 +17,4 @@ class Solution:
             node1.left = merge(node1.left, node2.left)
             node1.right = merge(node1.right, node2.right)
             return node1
-        merge(root1, root2)
-        return root1
+        return merge(root1, root2)
