0b5d97e8f435af5883c524e138553bd1a0ac2868
seongmook lee <43744260+2012monk@users.noreply.github.com>
2021-08-10 17:25:01 +0900

    Time: 56 ms (85.84%), Space: 15.6 MB (59.44%) - LeetHub

--- convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.py
+++ convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.py
@@ -5,30 +5,16 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def search(self, arr, t):
-        l, r = 0, len(arr) - 1
-        while l <= r:
-            m = (l+r)//2
-            if arr[m] <= t:
-                r = m - 1
-            else:
-                l = m + 1
-        return arr[l]
-                
-    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
-        mid = (len(nums) + 1) // 2
-        def build(arr):
-            if not arr:
-                return
-            if len(arr) == 1:
-                return TreeNode(arr[0])
-            
-            mid = (len(arr) - 1) // 2
-            node = TreeNode(arr[mid])
-            
-            node.left = build(arr[:mid])
-            node.right = build(arr[mid+1:])
-            return node
-        return build(nums)
+    def sortedArrayToBST(self, arr: List[int]) -> Optional[TreeNode]:
+        if not arr:
+            return
+        if len(arr) == 1:
+            return TreeNode(arr[0])
+        mid = (len(arr) - 1) // 2
+        node = TreeNode(arr[mid])
+
+        node.left = self.sortedArrayToBST(arr[:mid])
+        node.right = self.sortedArrayToBST(arr[mid+1:])
+        return node
         
             
