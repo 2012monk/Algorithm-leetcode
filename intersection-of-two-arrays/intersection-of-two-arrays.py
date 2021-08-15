8274706a2f792d01ae1e94fd8fe6fad3992e7f83
seongmook lee <43744260+2012monk@users.noreply.github.com>
2021-08-15 16:39:33 +0900

    Time: 44 ms (75.58%), Space: 14.6 MB (14.94%) - LeetHub

--- intersection-of-two-arrays/intersection-of-two-arrays.py
+++ intersection-of-two-arrays/intersection-of-two-arrays.py
@@ -1,28 +1,6 @@
 class Solution:
-    def search(self, a, t):
-        l,r = 0,len(a)-1
-        while l <= r:
-            m = (l+r)//2
-            if a[m] == t:
-                return m
-            if a[m] > t:
-                r = m - 1
-            else:
-                l = m + 1
-        return -1
-        
     def intersection(self, n1: List[int], n2: List[int]) -> List[int]:
-        # n1,n2 = sorted(list(set(n1))), sorted(list(set(n2)))
-        # if len(n1) > len(n2):
-        #     n1, n2 = n2, n1
-        # res = set()
-        # for k in set(n1):
-        #     i = bisect.bisect_left(n2, k)
-        #     if 0 <= i < len(n2):
-        #         res.add(k)
-        # return list(res)
         n1, n2 = set(n1), set(n2)
-        res =[]
         if len(n1) > len(n2):
             n1,n2=n2,n1
         return [k for k in n1 if k in n2]
