c8921861d3209be01c327cc131a73bb805695812
seongmook lee <43744260+2012monk@users.noreply.github.com>
2021-08-15 16:53:01 +0900

    Time: 60 ms (83.70%), Space: 14.6 MB (63.18%) - LeetHub

--- two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -1,5 +1,16 @@
 class Solution:
+    def pointer(self, n, t):
+        l, r = 0, len(n) - 1
+        while l != r:
+            if n[l] + n[r] < t:
+                l += 1
+            elif n[l] + n[r] > t:
+                r -= 1
+            else:
+                return l+1, r+1
+            
     def twoSum(self, n: List[int], t: int) -> List[int]:
+        # return self.pointer(n, t)
         for i, k in enumerate(n):
             v = t - k
             j = bisect.bisect_left(n, v, i + 1)
