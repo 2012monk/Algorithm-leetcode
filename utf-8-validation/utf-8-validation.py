c8d130cdac2ef5d6c69b81d2d2d0011c73d7bdd5
seongmook lee <43744260+2012monk@users.noreply.github.com>
2021-08-16 21:24:00 +0900

    Time: 108 ms (65.56%), Space: 14.6 MB (62.43%) - LeetHub

---
+++ utf-8-validation/utf-8-validation.py
@@ -0,0 +1,28 @@
+class Solution:
+    def validUtf8(self, data: List[int]) -> bool:
+        def check(st, size):
+            for i in range(st + 1, st+size + 1):
+                if i >= len(data) or data[i] >> 6 != 0b10:
+                    return False
+            return True
+        # m0 = 0x40, m1 = 0xc0, m2 = 0xe0, m3 = 0xf0
+        
+        l = len(data)
+        i = 0
+        while i < l:
+            s = data[i]
+            if s >> 3 == 0b11110 and check(i, 3):
+                i += 4
+            elif s >> 4 == 0b1110 and check(i, 2):
+                i += 3
+            elif s >> 5 == 0b110 and check(i, 1):
+                i += 2
+            elif s >> 7 == 0:
+                i += 1
+            else:
+                return False
+            
+        return True
+        
+            
+        
