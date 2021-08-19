f5d4e51905b5ebd6c18073de64b6612e9fefddc7
seongmook lee <43744260+2012monk@users.noreply.github.com>
2021-08-19 19:16:38 +0900

    Time: 6 ms (71.10%), Space: 40 MB (59.83%) - LeetHub

--- queue-reconstruction-by-height/queue-reconstruction-by-height.java
+++ queue-reconstruction-by-height/queue-reconstruction-by-height.java
@@ -1,16 +1,15 @@
 class Solution {
     public int[][] reconstructQueue(int[][] people) {
-        PriorityQueue<int[]> q = new PriorityQueue<>(
-            (a, b) -> a[0] != b[0] ? -a[0]+b[0]:a[1]-b[1]);
-        List<int[]> li = new LinkedList<>();
-        
-        for (int[] p: people) q.offer(p);
-        
-        while (!q.isEmpty()) {
-            int[] a = q.poll();
-            li.add(a[1], a);
+        Arrays.sort(people, new Comparator<int[]>(){
+            @Override
+            public int compare(int[] a, int[] b) {
+                return a[0] == b[0] ? a[1]-b[1]: -a[0]+b[0];
+            }
+        });
+        LinkedList<int[]> li = new LinkedList<>();
+        for (int[] p:people) {
+            li.add(p[1], p);
         }
-        
         return li.toArray(new int[people.length][]);
     }
 }
