d535ba22d11cd310267407542564f8fee531bc20
seongmook lee <43744260+2012monk@users.noreply.github.com>
2021-08-19 20:40:45 +0900

    Time: 2 ms (98.75%), Space: 40.1 MB (67.80%) - LeetHub

--- task-scheduler/task-scheduler.java
+++ task-scheduler/task-scheduler.java
@@ -1,45 +1,15 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-        Map<Character, Integer> map = new HashMap<>();
+        int[] map = new int[26];
+        
         for (char c:tasks) {
-            map.put(c, map.getOrDefault(c, 0)+1);
+            map[c-'A']++;
         }
         
-        PriorityQueue<int[]> q = new PriorityQueue<>(new Comparator<int[]>(){
-            @Override
-            public int compare(int[] a, int[] b) {
-                return a[0]-b[0];
-            }
-        });
-        for (char c: map.keySet()) {
-            int a = map.get(c);
-            q.offer(new int[]{-a, (int) c});
-        }
-        int res = 0;
-        while (true) {
-            int[] v = new int[256];
-            int k = 0;
-            while (!q.isEmpty() && k <= n) {
-                int[] a = q.poll();
-                if (a[0] < 0) {
-                    v[a[1]] = a[0]+1;
-                }
-                res++;
-                k++;
-            }
-            for (int i=0;i<256;i++) {
-                if (v[i] < 0) {
-                    q.offer(new int[] {v[i], i});
-                    v[i]=0;
-                        
-               };
-            }
-            if (q.isEmpty()) {
-                break;
-            }
-            res += n - k + 1;
-        }
-        return res;
+        Arrays.sort(map);
+        int i=25;
+        while (i >= 0 && map[i] == map[25])i--;
         
+        return Math.max(tasks.length, (map[25] - 1)*(n + 1)+ 25 - i);
     }
 }
