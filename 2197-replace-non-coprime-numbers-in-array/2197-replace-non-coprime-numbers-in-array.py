8cc9b5418986bd3a65e5b398b46c28dc013fcb20
seongmook lee <43744260+2012monk@users.noreply.github.com>
2022-03-31 13:42:35 +0900

    Time: 2370 ms (53.59%), Space: 30.6 MB (21.62%) - LeetHub

--- 2197-replace-non-coprime-numbers-in-array/2197-replace-non-coprime-numbers-in-array.py
+++ 2197-replace-non-coprime-numbers-in-array/2197-replace-non-coprime-numbers-in-array.py
@@ -1,13 +1,5 @@
 import collections
 class Solution:
-    def isCoprime(self, n1, n2):
-        return math.gcd(n1, n2) == 1
-    def isHaveNonCoprime(self, arr):
-        for i in range(len(arr) - 1):
-            if not self.isCoprime(arr[i], arr[i + 1]):
-                return True
-        return False
-            
     def replaceNonCoprimes(self, nums: List[int]) -> List[int]:
         st = []
         for n in nums:
