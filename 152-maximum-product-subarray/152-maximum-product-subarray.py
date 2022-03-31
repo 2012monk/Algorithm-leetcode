a90581d96985fc4f73e11fc25e45a860386f12f8
seongmook lee <43744260+2012monk@users.noreply.github.com>
2022-03-31 13:01:42 +0900

    Time: 82 ms (95.31%), Space: 15.6 MB (8.28%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.py
+++ 152-maximum-product-subarray/152-maximum-product-subarray.py
@@ -1,7 +1,12 @@
 class Solution:
-    def maxProduct(self, nums: List[int]) -> int:
-        rev=  nums[::-1]
-        for i in range(1, len(nums)):
-            nums[i] *= nums[i - 1] or 1
-            rev[i] *= rev[i - 1] or 1
-        return max(nums+rev)
+    def maxProduct(self, prefix_product: List[int]) -> int:
+        """
+        배열에 0 인 요소가 없다면 가장 큰 곱의 부분배열은 첫 요소 또는 마지막 요소 부터 시작한다
+        그렇기에 prefix, suffix 를 미리 계산한 배열의 최댓값이 답이 된다.
+        만약 배열에 0 인 요소가 있다면 0을 중심으로 여러개의 부분배열로 나누어 계산한다
+        """
+        suffix_product = prefix_product[::-1]
+        for i in range(1, len(prefix_product)):
+            prefix_product[i] *= prefix_product[i - 1] or 1
+            suffix_product[i] *= suffix_product[i - 1] or 1
+        return max(prefix_product+suffix_product)
