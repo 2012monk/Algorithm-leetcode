9c00a2adcc1113b3c26eef2301a0c0cc011c9aa7
seongmook lee <43744260+2012monk@users.noreply.github.com>
2022-06-13 18:57:03 +0900

    Time: 3 ms (72.60%), Space: 5.5 MB (86.63%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.c
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.c
@@ -1,14 +1,14 @@
 
+typedef long long ll;
 
 int myAtoi(char * s){
 
     int sign = 1;
-    long long ret = 0;
+    ll ret = 0;
     int max = __INT_MAX__;
     int min = -max - 1;
     while (*s == ' ') s++;
     if (*s == '-' || *s == '+') sign *= -(*(s++) - 44);
-    printf("%d",sign);
     while (*s >= '0' && *s <= '9') {
         ret = ret * 10;
         ret += *(s++) - '0';
