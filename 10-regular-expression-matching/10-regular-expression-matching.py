5d14dfc550a1bdd1dd619126d832d30665409bbb
seongmook lee <43744260+2012monk@users.noreply.github.com>
2022-06-15 14:06:28 +0900

    Time: 40 ms (97.04%), Space: 14.1 MB (36.32%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.py
+++ 10-regular-expression-matching/10-regular-expression-matching.py
@@ -1,22 +1,38 @@
-class Solution:
-    def isMatch(self, s: str, p: str) -> bool:
-        if not s or not p:
-            return 0
-        n,m = len(s),len(p)
-        dp = [[0]*(m+1) for _ in range(n + 1)]
-        dp[0][0] = 1
-        for i in range(m):
-            if p[i] == '*' and dp[0][i - 1]:
-                dp[0][i + 1] = 1
-        
-        for i in range(n):
-            for j in range(m):
-                if p[j] == '.' or s[i] == p[j]:
-                    dp[i + 1][j + 1] = dp[i][j]
-                if p[j] == '*':
-                    if p[j - 1] == '.' or s[i] == p[j - 1]:
-                        dp[i + 1][j + 1] = dp[i + 1][j] | dp[i][j + 1] | dp[i + 1][j - 1]
+sys.setrecursionlimit(10**6)
+
+#class Solution:
+#    def isMatch(self, s: str, p: str) -> bool:
+#        
+#        n,m=len(s),len(p)
+#        dp = {}
+#        def f(i, j):
+#            if j == m:
+#                dp[i,j] = i == n
+#                return dp[i, j]
+#            if (i, j) in dp:
+#                return dp[i, j]
+#            match = i < n and p[j] in ('.', s[i])
+#            if j < m - 1 and p[j + 1] == '*':
+#                dp[i,j] = f(i, j + 2) or match and f(i + 1, j)
+#            else:
+#                dp[i,j] = f(i + 1, j + 1) and match
+#            return dp[i,j]
+#        return f(0, 0)
+class Solution(object):
+    def isMatch(self, text, pattern):
+        memo = {}
+        def dp(i, j):
+            if (i, j) not in memo:
+                if j == len(pattern):
+                    ans = i == len(text)
+                else:
+                    first_match = i < len(text) and pattern[j] in {text[i], '.'}
+                    if j+1 < len(pattern) and pattern[j+1] == '*':
+                        ans = dp(i, j+2) or first_match and dp(i+1, j)
                     else:
-                        dp[i + 1][j + 1] = dp[i + 1][j - 1]
-        return dp[n][m]
-        
+                        ans = first_match and dp(i+1, j+1)
+
+                memo[i, j] = ans
+            return memo[i, j]
+
+        return dp(0, 0)
