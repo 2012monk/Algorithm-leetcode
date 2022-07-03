9f4a635ff103b4969627fd452714043c7dc29512
seongmook lee <43744260+2012monk@users.noreply.github.com>
2022-07-03 15:31:14 +0900

    Time: 70 ms (5.43%), Space: 13.7 MB (99.02%) - LeetHub

---
+++ 54-spiral-matrix/54-spiral-matrix.py
@@ -0,0 +1,26 @@
+class Solution:
+    def spiralOrder(self, g: List[List[int]]) -> List[int]:
+        
+        
+        m,n=len(g),len(g[0])
+        d=[[0,1],[1,0],[0,-1],[-1,0]]
+        i,j=0,0
+        c=0
+        t=m*n
+        dr=0
+        ret=[]
+        v=[[0]*m for _ in range(n)]
+        
+        while c < t:
+            ret.append(g[i][j])
+            g[i][j] = -101
+            x,y=i+d[dr][0],j+d[dr][1]
+            if x < 0 or x >=m or y<0 or y>=n or g[x][y] == -101:
+                dr = (dr+1) % 4
+            i,j=i+d[dr][0],j+d[dr][1]
+            c+=1
+        return ret
+        
+            
+            
+            
