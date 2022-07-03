d19667ef4fa3037f8b6dfc37b23d67d99d2540a5
seongmook lee <43744260+2012monk@users.noreply.github.com>
2022-07-03 14:18:55 +0900

    Time: 72 ms (29.79%), Space: 13.9 MB (36.01%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.py
+++ 376-wiggle-subsequence/376-wiggle-subsequence.py
@@ -2,13 +2,11 @@ class Solution:
     def wiggleMaxLength(self, a: List[int]) -> int:
         
         p=[]
-        mx=0
         for i in range(1, len(a)):
             if a[i] == a[i-1]:
                 continue
-            c = a[i]-a[i-1]
             v=1
-            if c<0:
+            if a[i]-a[i-1]<0:
                 v=-1
             if not p or p[-1] != v:
                 p.append(v)
