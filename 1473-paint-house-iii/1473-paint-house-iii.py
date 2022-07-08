dd11dbae358260af3e7d286d0434b24fa72169a0
seongmook lee <43744260+2012monk@users.noreply.github.com>
2022-07-08 15:15:02 +0900

    Time: 3251 ms (25.58%), Space: 44.2 MB (31.01%) - LeetHub

--- 1473-paint-house-iii/1473-paint-house-iii.py
+++ 1473-paint-house-iii/1473-paint-house-iii.py
@@ -2,10 +2,8 @@ class Solution:
     def minCost(self, houses: List[int], cost: List[List[int]], m: int, n: int, target: int) -> int:
         
         INF = 10**9
-        d={}
+        @functools.lru_cache(None)
         def f(x, g, p):
-            if (x,g,p) in d:
-                return d[x,g,p]
             if x >= m:
                 if g == 0:
                     return 0
@@ -15,7 +13,6 @@ class Solution:
             ret = INF
             for i in range(n):
                 ret = min(ret, f(x+1, g-+(i+1!=p), i+1) + cost[x][i])
-            d[x,g,p] =ret;
             return ret
 
         r = f(0, target, -1)
