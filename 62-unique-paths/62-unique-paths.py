501edd17efe99ee4c3b024d870a87e3d59d79edc
seongmook lee <43744260+2012monk@users.noreply.github.com>
2022-09-02 11:51:32 +0900

    Time: 63 ms (15.55%), Space: 14 MB (15.02%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -7,4 +7,13 @@ class Solution:
                 return 1
             return f(m - 1, n) + f(m, n - 1)
         
-        return f(m, n)
+        cache = {}
+        def g(m, n):
+            if m == 1 or n == 1:
+                return 1
+            if (m, n) in cache:
+                return cache[m,n]
+            cache[m, n] = g(m - 1, n) + g(m, n - 1)
+            return cache[m, n]
+        
+        return g(m, n)
